34dafc91593a49616f41214e98a51641ba98787a
Daniel Carvajal <carvajal.dev@hotmail.com>
2025-02-08 20:18:57 -0300

    atualizacao altura cards

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { useProduct } from "../hooks/useCardProduct";
 import dadosJson from '../mocks/dados2.json'
 
-const class1 = 'flex space-x-6 rounded-2xl px-3 border-b py-3 border-zinc-700 mt-3 last:border-none '
+const class1 = 'flex space-x-6 h-30 rounded-2xl px-3 py-3 border-zinc-700 mt-3 '
 
-const class2 = 'flex space-x-6 bg-amber-500  rounded-2xl px-3 border-b py-3 border-zinc-700 mt-3 last:border-none '
+const class2 = 'flex space-x-6 bg-amber-500 h-30  rounded-2xl px-3 border-b py-3 border-zinc-700 mt-3  '
 
 export const CardItem = () => {
 
@@ -17,7 +17,7 @@ export const CardItem = () => {
         dadosJson[nameProduct].map((product, index) => (
           <div key={product.id} 
           onClick={() =>setItemBg(product.id)}
-          className={`  ${itemBg == product.id  ? class2 : class1}`}>
+          className={` flex  items-center ${itemBg == product.id  ? class2 : class1}`}>
 
             <div>
               <img className="w-24 rounded-2xl " src={product.img} alt={product.name} />
@@ -27,7 +27,7 @@ export const CardItem = () => {
               <h2 className={`${itemBg === product.id ? 'uppercase font-bold text-black tracking-wide' : 'uppercase font-bold  text-mint-500 tracking-wide'}`}>{product.name}</h2>
               <p className="text-sm w-60">{product.dsc}</p>
 
-              <div className="flex absolute right-5 mt-3 rounded-md text-black px-3 justify-end font-medium bg-mint-500">
+              <div className="flex absolute right-5  rounded-md text-black px-3 justify-end font-medium bg-mint-500">
                 R$ {product.price.toFixed(2)}
               </div>
             </div>
